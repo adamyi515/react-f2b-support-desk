18d20d16b17626d124dd8f259f0bc4801406146c
0mni-gears515 <adamyi515@gmail.com>
2022-10-26 14:37:39 -0500

    Basic node configuration.

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-console.log('Server...');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello'
+    });
+})
+
+
+
+
+app.listen(PORT, () => console.log('SERVER started on: ' + PORT));
